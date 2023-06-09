b24e52000f43119a885ff74917ab04856095c86e
Sofia <rybina.sofia@gmail.com>
2023-06-09 11:32:57 +0300

    Две задачи

---
+++ task0/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.WriteLine("Input number:");
+int number = int.Parse(Console.ReadLine()!); //!убирает предупреждение, но не решает проблему
+int result = number * number;
+Console.WriteLine("Square number = " + result);
