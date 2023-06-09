9edf0bf4b70edfdbf859ed84c305d34cddea2164
Sofia <rybina.sofia@gmail.com>
2023-06-09 21:35:29 +0300

    Задача 1-2,5,7

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,2 +1,55 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Задача №1. Напишите программу, которая на вход принимает два числа и проверяет, является ли первое число квадратом второго.
+a = 25, b = 5 -> да 
+a = 2, b = 10 -> нет 
+a = 9, b = -3 -> да 
+a = -3 b = 9 -> нет*/
+/*
+Console.WriteLine("Input number1:");
+int number1 = int.Parse(Console.ReadLine()!); 
+Console.WriteLine("Input number2:");
+int number2 = int.Parse(Console.ReadLine()!); 
+
+if (number1 == number2 * number2)
+    {
+        Console.WriteLine("Yes");
+    }
+    else
+    {
+        Console.WriteLine("No");
+    }
+*/
+
+/*Задача №3. Напишите программу, которая будет выдавать название дня недели по заданному номеру.
+3 -> Среда 
+5 -> Пятница*/
+/*
+Console.WriteLine("Input number:");
+int number1 = int.Parse(Console.ReadLine()!); 
+
+if (number1 == 3)
+    {
+        Console.WriteLine("->Wednesday");
+    }
+    else if (number1 == 5 )
+    {
+        Console.WriteLine("->Friday");
+    }
+    else
+        {
+            Console.WriteLine("Unknown day!");
+        }
+*/
+
+/*Задача №5. Напишите программу, которая на вход принимает одно число (N), а на выходе показывает все целые числа в промежутке от -N до N.
+4 -> "-4, -3, -2, -1, 0, 1, 2, 3, 4" 
+2 -> " -2, -1, 0, 1, 2"*/
+
+Console.WriteLine("Input number:");
+int number = int.Parse(Console.ReadLine()!); 
+int length = number-number-number;
+
+
+/*Задача №7. Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает последнюю цифру этого числа.
+	456 -> 6
+	782 -> 2
+	918 -> 8*/
