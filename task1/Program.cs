828577ef00f0e069d1228dc6fe1ce54d1f215754
Sofia <rybina.sofia@gmail.com>
2023-06-09 21:47:53 +0300

    Задача 1-2,5,7

--- task1/Program.cs
+++ task1/Program.cs
@@ -43,13 +43,33 @@ if (number1 == 3)
 /*Задача №5. Напишите программу, которая на вход принимает одно число (N), а на выходе показывает все целые числа в промежутке от -N до N.
 4 -> "-4, -3, -2, -1, 0, 1, 2, 3, 4" 
 2 -> " -2, -1, 0, 1, 2"*/
-
-Console.WriteLine("Input number:");
+/*
+Console.WriteLine("Input number N:");
 int number = int.Parse(Console.ReadLine()!); 
-int length = number-number-number;
-
+int i = -number;
+while (i <= number)
+{
+    Console.Write(i + " ");
+    i++;
+}
+*/
 
 /*Задача №7. Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает последнюю цифру этого числа.
 	456 -> 6
 	782 -> 2
 	918 -> 8*/
+
+Console.WriteLine("Input three digit number:");
+int number = int.Parse(Console.ReadLine()!); 
+if(100 <= number&&number < 1000)
+{
+    int result = number % 10;
+    Console.Write(result);
+}
+else if(100 > number)
+{
+    Console.Write("Number too small!");
+}
+else {
+    Console.Write("Number too large!");
+}
